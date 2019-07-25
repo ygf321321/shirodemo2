9677c1846cfd5659d0c80f1dd192689f49306006
ygf321321 <ygf123456>
2019-07-25 16:56:09 +0800

    1

---
+++ src/main/java/com/gf/shirodemo2/Shirodemo2Application.java
@@ -0,0 +1,13 @@
+package com.gf.shirodemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shirodemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shirodemo2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/gf/shirodemo2/service/UserInfoService.java
@@ -0,0 +1,11 @@
+package com.gf.shirodemo2.service;
+
+import com.gf.shirodemo2.bean.UserInfo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserInfoService {
+    public UserInfo findByUsername(String username){
+        return null;
+    };
+}
